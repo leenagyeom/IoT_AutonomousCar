a9eb785af524773966c70a9ebb0e83c712d9609c
leenagyeom91 <leenagyeom91@gmail.com>
2019-04-24 03:01:54 +0900

    아두이노 최적화

--- test/arduino.ino
+++ test/arduino.ino
@@ -1,101 +1,62 @@
-int in1Pin = 6;      // 뒷바퀴
-int in2Pin = 7;      // 뒷바퀴
-int in3Pin = 8;      // 앞바퀴
-int in4Pin = 9;      // 앞바퀴
-int SpeedPin_F = 2;    // 앞바퀴 속도
-int SpeedPin_B = 3;    // 뒷바퀴 속도
-int echoPin=4;		// 초음파
-int trigPin=5;		// 초음파
+int in1Pin = 6;      // back wheel 1
+int in2Pin = 7;      // back wheel 2
+int in3Pin = 8;      // front wheel 3
+int in4Pin = 9;      // front wheel 4
+int SpeedPin_34 = 2;    // front speed
+int SpeedPin_12 = 3;    // back speed
+int turn=0;
 
 void setup() {
-	Serial.begin(9600);
-	pinMode(in1Pin, OUTPUT);             // 제어 1번핀 출력모드 설정
-	pinMode(in2Pin, OUTPUT);             // 제어 2번핀 출력모드 설정
-	pinMode(SpeedPin_B, OUTPUT);            // PWM제어핀 출력모드 설정
-	pinMode(in3Pin, OUTPUT);             // 제어 3번핀 출력모드 설정
-	pinMode(in4Pin, OUTPUT);             // 제어 4번핀 출력모드 설정
-	pinMode(SpeedPin_F, OUTPUT);            // PWM제어핀 출력모드 설정
-	pinMode(trigPin,OUTPUT);
-	pinMode(echoPin,INPUT);				// 초음파 들어와야함
+  Serial.begin(9600);
+  pinMode(in1Pin, OUTPUT);
+  pinMode(in2Pin, OUTPUT);
+  pinMode(SpeedPin_12, OUTPUT);
+  pinMode(in3Pin, OUTPUT);
+  pinMode(in4Pin, OUTPUT);
+  pinMode(SpeedPin_34, OUTPUT);
 }
-
+ 
 void loop() {
-	// float distance=ultrasonic();
-
-	//  Serial.println(distance);
-	// if(distance<30)
-	//   StopM();
-	//  else
-	ForwardM();
-
-	if(Serial.available() > 0){			// 라즈베리에서 신호들어오면
-		int turn = Serial.parseInt();
-		Serial.println(turn);
-		if(turn == 1)
-		RightM();
-		if(turn == 2)
-		LeftM();
-	}
-	midM();
-
+     ForwardM();
+    
+    if(Serial.available() > 0){ 
+      turn = Serial.parseInt();
+      Serial.println(turn);
+      if(turn == 1 | turn == 11){
+          RightM(300);
+          midM();
+      }
+      if(turn == 2 | turn == 22){
+          LeftM(1000);
+          midM();
+      }
+    }   
 }
-
-float ultrasonic(void)
-{
-	digitalWrite(trigPin,LOW);
-	digitalWrite(echoPin,LOW);
-	delayMicroseconds(2);
-	digitalWrite(trigPin,HIGH);
-	delayMicroseconds(10);
-	digitalWrite(trigPin,LOW);
-
-	unsigned long duration = pulseIn(echoPin,HIGH);	// 에코핀이 high 유지한 시간(us) 리턴됨
-													// pulseIn(echoPin, High) pin은 입력모드, value는 포트의 레벨 지정
-	float distance=((float)(340*duration)/10000)/2;	// high시간으로 거리 계산 (초음파가 나갔다가 다시 돌아오는 시간)
-													// 왕복거리이므로 2로 나눈다, 초음파 속도는 340m/s
-													// duration의 값(high 유지한 시간)은 us단위이므로 s단위로 바꿔준다. (us -> s) duration / 10^6
-													// 340m = 34000cm
-													// 거리(cm) = (34000(cm/s) * duration(s) / 10^6)/2
-													//            340*duration / 10000 / 2
-	return distance;
-} 
 
 void ForwardM()
 {
-	digitalWrite(in1Pin, LOW);			//모터가 정방향으로 회전
-	digitalWrite(in2Pin, HIGH);
-	analogWrite(SpeedPin_B, 150);		//모터 속도를 최대로 설정
+  digitalWrite(in1Pin, LOW);
+  digitalWrite(in2Pin, HIGH);
+  analogWrite(SpeedPin_12, 120);
 }
 
-void StopM()
-{
-	digitalWrite(in1Pin, LOW);			//모터가 정방향으로 회전
-	digitalWrite(in2Pin, LOW);
-	analogWrite(SpeedPin_B, 100);		//모터 속도를 최대로 설정
+void LeftM(int timed){
+  digitalWrite(in3Pin, LOW);
+  digitalWrite(in4Pin, HIGH);
+  analogWrite(SpeedPin_34, 250);
+  delay(timed);
 }
 
-void LeftM(){
-	digitalWrite(in1Pin, LOW);			//모터가 정방향으로 회전
-	digitalWrite(in2Pin, HIGH);
-	analogWrite(SpeedPin_B, 150);
-	digitalWrite(in3Pin, LOW);			//모터가 정방향으로 회전
-	digitalWrite(in4Pin, HIGH);
-	analogWrite(SpeedPin_F, 250);		// 250 : 조향 각을 크게
-	delay(1000);
-}
-
-void RightM(){
-	digitalWrite(in1Pin, LOW);			//모터가 정방향으로 회전
-	digitalWrite(in2Pin, HIGH);
-	analogWrite(SpeedPin_B, 150);		// 150 : 값이 크면 빠르게 달린다
-	digitalWrite(in3Pin, HIGH);			//모터가 역방향으로 회전
-	digitalWrite(in4Pin, LOW);
-	analogWrite(SpeedPin_F, 255);		// 255 : 조향 각을 크게
-	delay(1000);
+void RightM(int timed){ 
+  
+  digitalWrite(in3Pin, HIGH);
+  digitalWrite(in4Pin, LOW);
+  analogWrite(SpeedPin_34, 250);
+  delay(timed);   
 }
 
 void midM(){
-	digitalWrite(in3Pin, LOW);			// 앞바퀴에 힘 풀려고 (축을 중앙으로 위치)
-	digitalWrite(in4Pin, LOW);
-	analogWrite(SpeedPin_F, 150);
+  digitalWrite(in3Pin, LOW);
+  digitalWrite(in4Pin, LOW);
+  analogWrite(SpeedPin_34, 250);
 }
