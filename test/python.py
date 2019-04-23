926f05c8b080fd0a9e724b258661d470db0743ac
leenagyeom91 <leenagyeom91@gmail.com>
2019-04-24 02:59:00 +0900

    오른쪽 선만 검출하도록 수정 검출안되면 우회전(아두이노에 1전달) 검출되는데 각도가 좌측으로 기울어있으면 좌회전(아두이노에 2전달)

--- test/python.py
+++ test/python.py
@@ -4,6 +4,10 @@ import math
 import numpy as np
 import serial
 
+port = "/dev/ttyACM0"
+serialFromArduino = serial.Serial(port,9600)
+serialFromArduino.flushInput()
+
 def roi(img,vertices):
  mask=np.zeros_like(img)
  cv2.fillPoly(mask,vertices,255)
@@ -12,7 +16,7 @@ def roi(img,vertices):
 
 def angle(dy,dx):
     return math.atan2(dy,dx)*180/math.pi
-
+    
 cap = cv2.VideoCapture(0)
 
 while (True):
@@ -27,30 +31,20 @@ while (True):
 
     if lines is not None:
         a,b,c = lines.shape
-        (d,e)=(row/2,row/2)
-            
-        for i in range(a):
-            (x1,y1,x2,y2) = (lines[i][0][0], lines[i][0][1], lines[i][0][2], lines[i][0][3])
-                
-            if max(y1,y2)>row/2 and min(x1,x2)<col/2 and angle(y2-y1,x2-x1)<0 and max(y1,y2)>d:
+        e=row/2    
 
-                d = max(y1,y2)
-                print("left Side Angle = ", angle(x2-x1,y2-y1))
-                cv2.line(src, (x1,y1), (x2,y2), (0, 0, 255), 3, cv2.LINE_AA)                
-                degree = angle(x2-x1,y2-y1)
-                if degree < 118:
-                    print("1")
-                   # serialFromArduino.write(b'1')
+        (x1,y1,x2,y2) = (lines[0][0][0], lines[0][0][1], lines[0][0][2], lines[0][0][3])
                 
-            if max(y1,y2)>row/2 and min(x1,x2)>col/2 and angle(y2-y1,x2-x1)>0 and max(y1,y2)>e:
+        if max(y1,y2)>row/2 and min(x1,x2)>col/2 and angle(y2-y1,x2-x1)>0 and max(y1,y2)>e:
+            e = max(y1,y2)
+            print("Right Side Angle = ", angle(x2-x1,y2-y1))
+            degree = angle(x2-x1,y2-y1)
+            cv2.line(src, (x1,y1), (x2,y2), (0, 0, 255), 3, cv2.LINE_AA) 
+            if degree > 65:
+                serialFromArduino.write(b'2')
 
-                e = max(y1,y2)
-                print("Right Side Angle = ", angle(x2-x1,y2-y1))
-                cv2.line(src, (x1,y1), (x2,y2), (0, 0, 255), 3, cv2.LINE_AA)
-                degree = angle(x2-x1,y2-y1)
-                if degree > 65:
-                    print("2")
-                   # serialFromArduino.write(b'2')
+    else:
+        serialFromArduino.write(b'1')
 
     cv2.imshow("detected lines", src)
     if cv2.waitKey(1) & 0xFF == ord('q'):
